7de033fd99c6ae2c8da2c9a87f04a5054c6b0ee8
joy016 <jhoylejas@gmail.com>
2023-08-07 05:20:50 +0800

    Copy to clipboard function implemented

--- Google-Drive-Download_Link-Generator/script.js
+++ Google-Drive-Download_Link-Generator/script.js
@@ -5,27 +5,62 @@ const embedVideo = document.querySelector('.embed-video-container');
 const inputLink = document.querySelector('#google-drive-link');
 const btnGenerate = document.querySelector('.btn-generate');
 const downLoadedLink = document.getElementById('downloaded-link');
+const embededAudio = document.getElementById('audio-link');
+const embededVideo = document.getElementById('embeded-video');
+const btnCopyDownloadedLInk = document.getElementById('copy-downloadedLink');
+const btnCopyVideo = document.getElementById('copy-videoLink');
+const copyAudio = document.getElementById('copy-audio');
+
+let videoIFrame = '';
+let audioEmbed = '';
+
+const copyToClipboard = (element) => {
+  const textArea = document.createElement('textarea');
+  textArea.value = element.value || element.innerText;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textArea);
+  alert('Copied to clipboard');
+};
 
 const handleGenerateLink = () => {
   const googleDriveLinkPattern =
     /^https:\/\/drive\.google\.com\/file\/d\/[a-zA-Z0-9_-]+\/view\?usp=drive_link$/;
-
   const url = new URL(inputLink.value);
   const originURL = url.origin;
   const urlId = inputLink.value.split('/');
   const addedLink = 'uc?export=download&id=';
-
   const combinedLink = `${originURL}/${addedLink}${urlId[5]}`;
+  const embedVideoSource = `${originURL}/file/d/${urlId[5]}/preview `;
+  videoIFrame = `<iframe width="420" height="315"src="${embedVideoSource}"></iframe>`;
+  audioEmbed = `<audio controls autoplay>
+  <source src="${combinedLink}" type="audio/mp3">
+</audio>`;
 
   if (!googleDriveLinkPattern.test(inputLink.value)) {
     alert('Invalid Google drive link');
     return;
   }
 
-  downLoadedLink.value += combinedLink;
+  downLoadedLink.value = combinedLink;
+  embededVideo.value = videoIFrame;
+  embededAudio.value = audioEmbed;
 
-  // alert(combinedLink);
+  console.log(audioEmbed);
 };
+
+btnCopyDownloadedLInk.addEventListener('click', () => {
+  copyToClipboard(downLoadedLink);
+});
+
+btnCopyVideo.addEventListener('click', () => {
+  copyToClipboard(videoIFrame);
+});
+
+copyAudio.addEventListener('click', () => {
+  copyToClipboard(audioEmbed);
+});
 
 btnClick.forEach((button) => {
   button.addEventListener('click', () => {
